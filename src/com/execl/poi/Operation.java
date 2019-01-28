2e2fd3c46136c0f5610dfee65c407822a3517a81
XeelongLee <1450844869@qq.com>
2019-01-28 16:36:39 +0800

    create and read xls or xlsx file

--- src/com/execl/poi/Operation.java
+++ src/com/execl/poi/Operation.java
@@ -7,6 +7,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  * 操作Excel 操作excel比较繁琐，正在努力学习中
@@ -18,13 +19,18 @@ public class Operation {
 	
 	public static void main(String[] args) throws Exception {
 		Operation obj = new Operation();
-		obj.opreation("E:\\WorkFile\\Java\\Test\\execl.xls");
+//		obj.createXls("E:\\WorkFile\\Java\\Test\\execl.xls");
+		obj.createXlsx("E:\\WorkFile\\Java\\Test\\execl.xlsx");
 	}
-	
-	public void opreation(String path) throws Exception {
-		/**
-		 * 操作.xls文件使用HSSF包
-		 */
+	/**
+	 * 创建.xls文件使用HSSF包
+	 */
+	public void createXls(String path) throws Exception {
+		String suffix = path.substring(path.lastIndexOf("."));
+		if(!suffix.equals(".xls")) {
+			System.out.println("请输入.xls文件");
+			return;
+		}
 		//创建一个工作簿
 		Workbook doc = new HSSFWorkbook(); 
 		File file = new File(path);
@@ -40,11 +46,62 @@ public class Operation {
 		Row rowTow = sheetTow.createRow(0);//创建第一行
 		rowTow.createCell(0).setCellValue(1);//在第一行中创建第一个单元格，值为int类型
 		rowTow.createCell(1).setCellValue("String");//在第一行中创建第二个单元格，值为String类型
-		rowTow.createCell(2).setCellValue(true);//在第第一行中创建第3个单元格，值为boolean类型
+		rowTow.createCell(2).setCellValue(true);//在第一行中创建第3个单元格，值为boolean类型
 		//输出到指定位置
 		FileOutputStream output = new FileOutputStream(file); 
 		//输出
 		doc.write(output);
 		output.close();
 	}
+	/**
+	 * 尝试读取.xls文件内容
+	 */
+	public void readXls(String path) {
+		String suffix = path.substring(path.lastIndexOf("."));
+		if(!suffix.equals(".xls")) {
+			System.out.println("请输入.xls文件");
+			return;
+		}
+		//创建一个工作簿
+		Workbook doc = new HSSFWorkbook(); 
+		File file = new File(path);
+		if(file.exists()) {
+			//若存在重复文件则删除
+			file.delete();
+		}
+	}
+	/**
+	 * 创建.xlsx文件使用XSSF包
+	 */
+	public void createXlsx(String path) throws Exception {
+		String suffix = path.substring(path.lastIndexOf("."));
+		if(!suffix.equals(".xlsx")) {
+			System.out.println("请输入.xlsx文件");
+			return;
+		}
+		Workbook doc = new XSSFWorkbook();
+		File file = new File(path);
+		if(file.exists()) {
+			//若存在重复文件则删除
+			file.delete();
+		}
+		//创建工作簿
+		Sheet sheetOne = doc.createSheet("One");
+		//对第一个sheet进行操作
+		Row row1 = sheetOne.createRow(0);//创建行1
+			//往第一行添加单元格并设置值
+			row1.createCell(0).setCellValue(1);
+			row1.createCell(1).setCellValue("String");
+			row1.createCell(2).setCellValue(false);
+		Row row2 = sheetOne.createRow(1);//创建行2
+			//往第一行添加单元格并设置值
+			row2.createCell(0).setCellValue(false);
+			row2.createCell(1).setCellValue("String");
+			row2.createCell(2).setCellValue(1);
+		doc.createSheet("Two");
+		FileOutputStream output = new FileOutputStream(file);
+		doc.write(output);
+		output.close();
+	}
+
 }
